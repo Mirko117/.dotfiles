a71b8b7e38eec27a056f108cede83f5335e5b34d
Mirko <suvajac117@gmail.com>
2026-01-20 17:08:51 +0100

    Added KDE shortcuts to stow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,21 @@
 set -e  # stop on error
 
 echo "[*] Stowing dotfiles..."
-stow kitty
-stow oh-my-posh
-stow zsh
+
+stow --restow kitty
+stow --restow oh-my-posh
+stow --restow zsh
+stow --restow kde
 
 echo "[*] Done!"
+
+echo "[*] Applying KDE files..."
+
+kquitapp6 kglobalaccel || true
+kstart6 kglobalaccel
+
+kquitapp6 plasmashell || true
+kstart6 plasmashell
+
+echo "[*] Done!"
+
